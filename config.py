f8b486cfe98d765123a1be531d97f94147a98f98
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-03-15 04:56:13 -0700

    update: fixed up config.py a little bit

--- config.py
+++ config.py
@@ -9,22 +9,31 @@ config = {
     "playerBlockDamage": 50,
     "playerCritDamage": 50,
     "playerHealAmount": 50,
-    "playerCritHeal": "100"
+    "playerCritHeal": 100
 }
 
 print(" Default config: ")
 print("", config)
 
+def get_user_input(prompt):
+  while True:
+    try:
+      return int(input(prompt))
+    except ValueError:
+      print("Please enter a valid integer.")
 
-config["maxPlayerHealth"] = input("\n\n Enter max player health: ")
-config["playerAttackDamage"] = input(" Enter attack damage: ")
-config["playerChargedAttackDamage"] = input(" Enter charged attack damage: ")
-config["playerBlockDamage"] = input(" Enter block resistance: ")
-config["playerCritDamage"] = input(" Enter crit damage: ")
-config["playerHealAmount"] = input(" Enter heal amount: ")
-config["playerCritHeal"] = input(" Enter crit heal amount: ")
 
-with open(f"{os.getcwd()}\\files\\config.json", "w") as file:
+config["maxPlayerHealth"] = get_user_input("\n\n Enter max player health: ")
+config["playerAttackDamage"] = get_user_input(" Enter attack damage: ")
+config["playerChargedAttackDamage"] = get_user_input(" Enter charged attack damage: ")
+config["playerBlockDamage"] = get_user_input(" Enter block resistance: ")
+config["playerCritDamage"] = get_user_input(" Enter crit damage: ")
+config["playerHealAmount"] = get_user_input(" Enter heal amount: ")
+config["playerCritHeal"] = get_user_input(" Enter crit heal amount: ")
+
+file_path = os.path.join(os.getcwd(), "files", "config.json")
+
+with open(os.path.join(os.getcwd(), 'files', 'config.json'), "w") as file:
     json.dump(config, file, indent = 4)
     
 print("Success!")
